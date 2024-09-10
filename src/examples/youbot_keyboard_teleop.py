d1e30abb421fd9ccb0ae94f3dbef9fe131d12863
TienNguyen <htienguyen181@gmail.com>
2024-09-10 16:26:41 +0700

    Fixed launch files and minor errors

--- src/examples/youbot_keyboard_teleop.py
+++ src/examples/youbot_keyboard_teleop.py
@@ -14,9 +14,9 @@ msg = """
 Reading from the keyboard  and Publishing to Twist!
 ---------------------------
 Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
+    u    i    o
+    j    k    l
+    m    ,    .
 
 q/z : increase/decrease max speeds by 10%
 w/x : increase/decrease only linear speed by 10%
@@ -36,7 +36,7 @@ moveBindings = {
 		',':(-1,0,0), 	# backward
 		'.':(0,0,-1), 	# turn right on spot
 		'm':(0,0,1), 	# turn left on spot
-	       }
+	        }
 
 speedBindings={
 		'q':(1.1,1.1),
@@ -45,7 +45,7 @@ speedBindings={
 		'x':(.9,1),
 		'e':(1,1.1),
 		'c':(1,.9),
-	      }
+	        }
 
 class TimeoutException(Exception): 
     pass 
@@ -59,13 +59,13 @@ def getKey():
     tty.setraw(sys.stdin.fileno())
     select.select([sys.stdin], [], [], 0)
     try:
-       key = sys.stdin.read(1)
-       #print "Read key"
+        key = sys.stdin.read(1)
+        #print "Read key"
     except TimeoutException:
-       #print "Timeout"
-       return "-"
+        #print "Timeout"
+        return "-"
     finally:
-       signal.signal(signal.SIGALRM, old_handler)
+        signal.signal(signal.SIGALRM, old_handler)
 
     signal.alarm(0)
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
@@ -78,59 +78,58 @@ def vels(speed,turn):
 	return "currently:\tspeed %s\tturn %s " % (speed,turn)
 
 if __name__=="__main__":
-    	settings = termios.tcgetattr(sys.stdin)
-	
-	pub = rospy.Publisher('cmd_vel', Twist)
-	rospy.init_node('teleop_twist_keyboard')
+    settings = termios.tcgetattr(sys.stdin)	
+    pub = rospy.Publisher('cmd_vel', Twist)
+    rospy.init_node('teleop_twist_keyboard')
 
-	x = 0
-	y = 0
-	th = 0
-	status = 0
+    x = 0
+    y = 0
+    th = 0
+    status = 0
 
-	try:
-		print msg
-		print vels(speed,turn)
-		while(1):
-			key = getKey()
-			if key in moveBindings.keys():
-				x = moveBindings[key][0]
-				y = moveBindings[key][1]
-				th = moveBindings[key][2]
-			elif key in speedBindings.keys():
-				speed = speed * speedBindings[key][0]
-				turn = turn * speedBindings[key][1]
+    try:
+        print(msg)
+        print(vels(speed,turn))
+        while(1):
+            key = getKey()
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][1]
+                th = moveBindings[key][2]
+            elif key in speedBindings.keys():
+                speed = speed * speedBindings[key][0]
+                turn = turn * speedBindings[key][1]
 
-				print vels(speed,turn)
-				if (status == 14):
-					print msg
-				status = (status + 1) % 15
-			else:
-				x = 0
-				y = 0
-				th = 0
-				if (key == '\x03'):
-					break
+                print(vels(speed,turn))
+                if (status == 14):
+                    print msg
+                status = (status + 1) % 15
+            else:
+                x = 0
+                y = 0
+                th = 0
+                if (key == '\x03'):
+                    break
 
-			twist = Twist()
-			twist.linear.x = x*speed 
-			twist.linear.y = y*speed 
-			twist.linear.z = 0
+            twist = Twist()
+            twist.linear.x = x*speed 
+            twist.linear.y = y*speed 
+            twist.linear.z = 0
 
-			twist.angular.x = 0 
-			twist.angular.y = 0
-			twist.angular.z = th*turn
-			pub.publish(twist)
+            twist.angular.x = 0 
+            twist.angular.y = 0
+            twist.angular.z = th*turn
+            pub.publish(twist)
 
-	except:
-		print e
+    except:
+        print('e')
 
-	finally:
-		twist = Twist()
-		twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
-		twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
-		pub.publish(twist)
+    finally:
+        twist = Twist()
+        twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        pub.publish(twist)
 
-    		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
 
